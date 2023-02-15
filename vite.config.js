c452b9913fcb1646f79a29cd4b8649b47e37d4bd
Matthew Rose <47019415+scarletbane@users.noreply.github.com>
2023-02-14 17:05:27 -0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import htmlPurge from 'vite-plugin-html-purgecss';
 
 export default defineConfig({
     root: 'src',
-    server: {
-        hot: true
-    },
     build: {
         emptyOutDir: true,
         outDir: '../dist',
+    },
+    server: {
+        hot: true
     },
     plugins: [
         htmlPurge(),
